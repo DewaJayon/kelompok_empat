a7e978ff445d4b4caa9a68fda2ba389f92f38e71
DewaJayon <dewajayon3@gmail.com>
2024-05-19 19:32:48 +0800

    feat: create page

--- lib/pages/create_page.dart
+++ lib/pages/create_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:awesome_snackbar_content/awesome_snackbar_content.dart';
+import 'package:kelompok_empat/widgets/tombol.dart';
 
 class CreatePage extends StatefulWidget {
   const CreatePage({super.key});
@@ -8,14 +10,82 @@ class CreatePage extends StatefulWidget {
 }
 
 class _CreatePageState extends State<CreatePage> {
+  void _showSnackbar(String title, String message, ContentType contentType) {
+    final snackBar = SnackBar(
+      /// need to set following properties for best effect of awesome_snackbar_content
+      elevation: 0,
+      behavior: SnackBarBehavior.floating,
+      backgroundColor: Colors.transparent,
+      content: AwesomeSnackbarContent(
+        title: title,
+        message: message,
+
+        /// change contentType to ContentType.success, ContentType.warning or ContentType.help for variants
+        contentType: contentType,
+      ),
+    );
+
+    ScaffoldMessenger.of(context)
+      ..hideCurrentSnackBar()
+      ..showSnackBar(snackBar);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
-        title: const Text('Create Page'),
+        backgroundColor: Colors.black,
+        title: const Text(
+          'Tambah Produk Page',
+          style: TextStyle(color: Colors.white),
+        ),
       ),
-      body: const Center(
-        child: Text('Create Page'),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Nama Produk',
+                  labelStyle: TextStyle(color: Colors.white),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.white,
+                      width: 2,
+                    ),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.white,
+                      width: 2,
+                    ),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    ),
+                  ),
+                ),
+                style: const TextStyle(color: Colors.white),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Tombol(
+              onTap: () {
+                _showSnackbar(
+                  'Success',
+                  'Produk Berhasil',
+                  ContentType.success,
+                );
+              },
+              text: "Tambah",
+            ),
+          ],
+        ),
       ),
     );
   }
